fd988fd38305573de1110db24449917917296d09
diegoceccarelli <diego.ceccarelli@gmail.com>
2013-11-12 17:05:44 +0100

    fixed pom dependencies

--- bm25f-ranking/src/test/java/eu/europeana/solr/ZipFileUtils.java
+++ bm25f-ranking/src/test/java/eu/europeana/solr/ZipFileUtils.java
@@ -11,46 +11,49 @@ import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
+import org.apache.commons.compress.archivers.zip.ZipFile;
 import org.apache.solr.common.SolrInputDocument;
 import org.apache.solr.handler.loader.XMLLoader;
-import org.apache.tools.zip.ZipEntry;
-import org.apache.tools.zip.ZipFile;
 
 /**
  * Util class used to read zip files
  * 
  * @author Yorgos.Mamakis@ kb.nl
- *
+ * 
  */
-@SuppressWarnings({"unchecked","restriction"})
+@SuppressWarnings({ "unchecked", "restriction" })
 public class ZipFileUtils {
-	
+
 	/**
 	 * Read a list of solr xml documents from a zip file
-	 * @param path The path of the zip file
+	 * 
+	 * @param path
+	 *            The path of the zip file
 	 * @return A list of SolrInputDocuments for indexing
 	 */
-	public static List<SolrInputDocument> readDocuments(String path){
+	public static List<SolrInputDocument> readDocuments(String path) {
 		try {
-			
-			
+
 			ZipFile zFile = new ZipFile(path);
-			
-			Enumeration<ZipEntry> zipEntries = zFile.getEntries();
+
+			Enumeration<ZipArchiveEntry> zipEntries = zFile.getEntries();
 			List<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
-			while(zipEntries.hasMoreElements()){
-				ZipEntry zEntry = zipEntries.nextElement();
-				//Avoid directories if present
-				if(zEntry.isDirectory()){
+			while (zipEntries.hasMoreElements()) {
+				ZipArchiveEntry zEntry = zipEntries.nextElement();
+				// Avoid directories if present
+				if (zEntry.isDirectory()) {
 					continue;
 				}
-				//Read the file in memory
+				// Read the file in memory
 				InputStream in = zFile.getInputStream(zEntry);
 				XMLInputFactory factory = XMLInputFactory.newInstance();
 				XMLLoader loader = new XMLLoader();
-				//create an XML reader required to convert the document through the XMLLoader of solr
+				// create an XML reader required to convert the document through
+				// the XMLLoader of solr
 				XMLStreamReader reader = factory.createXMLStreamReader(in);
-				//get to the first START_ELEMENT (doc) as the XMLLoader does not handle START_DOC event types
+				// get to the first START_ELEMENT (doc) as the XMLLoader does
+				// not handle START_DOC event types
 				reader.next();
 				docs.add(loader.readDoc(reader));
 			}
@@ -66,8 +69,7 @@ public class ZipFileUtils {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
+
 		return null;
 	}
 }
