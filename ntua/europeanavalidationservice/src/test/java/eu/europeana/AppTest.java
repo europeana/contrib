1eeb6d7220912f942b433ca8a45c358b41118f1f
geomark <georgios.markakis@kb.nl>
2013-12-02 18:27:42 +0100

    Re #1088: Intial investigation prototype

---
+++ ntua/europeanavalidationservice/src/test/java/eu/europeana/AppTest.java
@@ -0,0 +1,91 @@
+package eu.europeana;
+
+import java.io.FileNotFoundException;
+
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.IndexHits;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    
+
+    
+    
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+
+    /**
+     * Rigourous Test :-)
+     * @throws FileNotFoundException 
+     */
+    public void parsefile() 
+    {
+    	GraphConstructor constr = new GraphConstructor();
+    	
+    	try {
+			constr.parseFile("src/test/resources/EDM_test_record-update.xml");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+        
+        assertTrue( true );
+    }
+    
+    
+    /**
+     * 
+     */
+    public void findResults(){
+    	GraphCrawler crawler = new GraphCrawler(); 
+    	crawler.totalItems("edm:WebResource");
+    	crawler.totalItems("edm:Agent");
+    	crawler.totalItems("edm:Place");
+    	crawler.totalItems("edm:TimeSpan");
+    	crawler.totalItems("skos:Concept"); 
+    	crawler.totalItems("ore:Aggregation");
+    	crawler.totalItems("ore:Proxy");
+    }
+    
+    public void traverse(){
+    	GraphCrawler crawler = new GraphCrawler();
+    	
+    	IndexHits<Node> items = crawler.totalItems("ore:Aggregation");
+    	
+
+		while(items.hasNext()){
+			Node node = items.next();
+			String path = crawler.nodesTraverser(node,2);
+			
+			System.out.println(path);
+			System.out.println("++++++++++++++++++++++++++++++");
+		}
+
+    	
+    }
+    
+
+}
