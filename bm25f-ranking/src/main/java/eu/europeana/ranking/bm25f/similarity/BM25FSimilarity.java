153a719a6d3a92fa62741b4f00ead4dfa2d09fc5
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-01-16 15:34:29 +0100

    remove cache and fix bug

--- bm25f-ranking/src/main/java/eu/europeana/ranking/bm25f/similarity/BM25FSimilarity.java
+++ bm25f-ranking/src/main/java/eu/europeana/ranking/bm25f/similarity/BM25FSimilarity.java
@@ -107,7 +107,6 @@ public class BM25FSimilarity extends Similarity {
 			float f = SmallFloat.byte315ToFloat((byte) i);
 
 			NORM_TABLE[i] = 1.0f / (f * f);
-			System.out.println(i + "\t-> " + NORM_TABLE[i]);
 		}
 	}
 
@@ -150,8 +149,7 @@ public class BM25FSimilarity extends Similarity {
 	public final ExactSimScorer exactSimScorer(SimWeight weight,
 			AtomicReaderContext context) throws IOException {
 		BM25FSimWeight w = (BM25FSimWeight) weight;
-		System.out.println("field: " + w.field);
-		System.out.println("norms " + context.reader().normValues(w.field));
+
 		return new BM25FExactSimScorer(w, context.reader().normValues(w.field));
 
 	}
@@ -254,14 +252,14 @@ public class BM25FSimilarity extends Similarity {
 		}
 
 		// compute freq-independent part of bm25 equation across all norm values
-		float cache[] = new float[256];
-		for (int i = 0; i < cache.length; i++) {
-			cache[i] = ((1 - bField) + bField * decodeNormValue((byte) i)
-					/ avgdl);
-			System.out.println("cache " + i + "\t" + cache[i]);
-		}
+		// float cache[] = new float[256];
+		// for (int i = 0; i < cache.length; i++) {
+		// cache[i] = ((1 - bField) + bField * decodeNormValue((byte) i)
+		// / avgdl);
+		// System.out.println("cache " + i + "\t" + cache[i]);
+		// }
 
-		return new BM25FSimWeight(field, idf, boost, avgdl, cache, k1);
+		return new BM25FSimWeight(field, idf, boost, avgdl, null, k1);
 	}
 
 	@Override
@@ -290,23 +288,35 @@ public class BM25FSimilarity extends Similarity {
 
 			this.norms = norms == null ? null : (byte[]) norms.getSource()
 					.getArray();
-			System.out.println("norms = " + this.norms);
 
 		}
 
 		@Override
 		public float score(int doc, int freq) {
+
+			// return queryBoost * freq / cache[norms[doc] & 0xFF];
+			float bField = params.getbParams().get(stats.field);
+			float boost = params.getBoosts().get(stats.field);
+			float num = freq * boost;
+
+			float den = 1 - bField;
 			if (norms == null) {
-				return queryBoost * freq
-						/ (1 - params.getbParams().get(stats.getField()));
+				logger.warn("no norms for field {} ", stats.field);
+			} else {
+
+				byte norm = this.norms[doc];
+				den += bField * decodeNormValue(norm) / stats.avgdl;
 			}
-			return queryBoost * freq / cache[norms[doc] & 0xFF];
+
+			if (den == 0)
+				return 0;
+			return num / den;
+
 		}
 
 		@Override
 		public Explanation explain(int doc, Explanation freq) {
-			System.out
-					.println("explain " + this.getClass() + " norms " + norms);
+
 			return explainScore(doc, freq, stats, norms,
 					score(doc, (int) freq.getValue()));
 		}
@@ -370,6 +380,24 @@ public class BM25FSimilarity extends Similarity {
 		boosts = params.getBoosts();
 		lengthBoosts = params.getbParams();
 		k1 = params.getK1();
+
+		// // return queryBoost * freq / cache[norms[doc] & 0xFF];
+		// float bField = params.getbParams().get(stats.field);
+		// float boost = params.getBoosts().get(stats.field);
+		// float num = freq * boost;
+		//
+		// float den = 1 - bField;
+		// if (norms == null) {
+		// logger.warn("no norms for field {} ", stats.field);
+		// } else {
+		//
+		// byte norm = this.norms[doc];
+		// den += bField * decodeNormValue(norm);
+		// }
+		// if (den == 0)
+		// return 0;
+		// return num / den;
+
 		Explanation result = new Explanation();
 		result.setDescription("score(doc=" + doc + ",field=" + stats.field
 				+ ", freq=" + freq + "), division of:");
@@ -377,8 +405,10 @@ public class BM25FSimilarity extends Similarity {
 		Explanation num = new Explanation();
 		num.setDescription(" numerator, product of: ");
 
-		Explanation boostExpl = new Explanation(stats.queryBoost, "boost["
-				+ stats.field + "]");
+		float boost = boosts.get(stats.field);
+
+		Explanation boostExpl = new Explanation(boost, "boost[" + stats.field
+				+ "]");
 
 		num.addDetail(freq);
 		num.addDetail(boostExpl);
@@ -392,25 +422,25 @@ public class BM25FSimilarity extends Similarity {
 
 		float length = -1;
 		Explanation fieldLength;
+		Explanation product = new Explanation();
+
 		if (norms != null) {
+			product.setDescription("denominator: ((1 - bField) + bField * length / avgFieldLength) :");
 			length = decodeNormValue(norms[doc]);
-			System.out.println("decode norm table norms[doc] " + norms[doc]
-					+ " length = " + length);
+
+			product.setValue((1 - b) + b * (length / stats.avgdl));
 
 			fieldLength = new Explanation(length, "length(" + stats.field + ")");
-			System.out.println("diego cane " + fieldLength.toHtml());
+			product.addDetail(fieldLength);
+			product.addDetail(averageLength);
+
 		} else {
-			fieldLength = new Explanation(-1, "NONORMS - length(" + stats.field
-					+ ")");
-			System.out.println("NO diego cane " + fieldLength.toHtml());
+			product.setDescription("[nofieldlength] denominator: ((1 - bField)");
+			product.setValue(1 - b);
+
 		}
 
-		Explanation product = new Explanation();
-		product.setDescription("denominator: ((1 - bField) + bField * length / avgFieldLength) :");
-		product.setValue((1 - b) + b * (length / stats.avgdl));
 		product.addDetail(bField);
-		product.addDetail(fieldLength);
-		product.addDetail(averageLength);
 
 		result.addDetail(num);
 		result.addDetail(product);
@@ -442,8 +472,6 @@ public class BM25FSimilarity extends Similarity {
 
 		@Override
 		public Explanation explain(int doc, Explanation freq) {
-			System.out
-					.println("explain " + this.getClass() + " norms " + norms);
 
 			return explainScore(doc, freq, stats, norms,
 					score(doc, freq.getValue()));
