fd300946d239d3a24876b9ad57f4c8d5b94389ff
diegoceccarelli <diego.ceccarelli@gmail.com>
2014-03-17 13:04:30 +0100

    add javadoc

--- query-logs-analysis/src/main/java/eu/europeana/querylog/learn/measure/Ndcg.java
+++ query-logs-analysis/src/main/java/eu/europeana/querylog/learn/measure/Ndcg.java
@@ -14,21 +14,6 @@
  *  limitations under the License.
  */
 
-/**
- *  Copyright 2014 Diego Ceccarelli
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 package eu.europeana.querylog.learn.measure;
 
 import java.util.HashMap;
@@ -39,6 +24,8 @@ import eu.europeana.querylog.QueryAssessment;
 import eu.europeana.querylog.QueryAssessment.RelevantDocument;
 
 /**
+ * Computes the Normalized Discounted Comulative Gain (NDCG).
+ * 
  * @author Diego Ceccarelli <diego.ceccarelli@isti.cnr.it>
  * 
  *         Created on Mar 11, 2014
@@ -54,6 +41,7 @@ public class Ndcg extends AbstractMeasure implements Measure {
 	@Override
 	public double match(List<String> results, QueryAssessment assessment) {
 		double idcg = idcg(results, assessment);
+		// this means that in the assessment there are no relevant results.
 		if (idcg == 0.0)
 			return 0.0;
 		return dcg(results, assessment) / idcg(results, assessment);
@@ -78,6 +66,9 @@ public class Ndcg extends AbstractMeasure implements Measure {
 		return dcg;
 	}
 
+	/**
+	 * compute the ideal discounted comulative gain
+	 */
 	private double idcg(List<String> results, QueryAssessment assessment) {
 		assessment.sort();
 		double idcg = 0d;
