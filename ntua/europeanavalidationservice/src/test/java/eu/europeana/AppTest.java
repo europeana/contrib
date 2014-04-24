7d769fe5f29ed36ca10828533e41607c773a9ff9
Nikolaos Simou <nsimou@image.ntua.gr>
2014-04-24 12:25:10 +0300

    Update of the libraries used.

--- ntua/europeanavalidationservice/src/test/java/eu/europeana/AppTest.java
+++ ntua/europeanavalidationservice/src/test/java/eu/europeana/AppTest.java
@@ -4,44 +4,49 @@ import java.io.FileNotFoundException;
 
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.index.IndexHits;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
+import org.junit.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
+
 public class AppTest 
-    extends TestCase
+//    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+//    /**
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
 
     
-
     
-    
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
 
 
     /**
      * Rigourous Test :-)
      * @throws FileNotFoundException 
+     * 
      */
+    @Test 
     public void parsefile() 
     {
     	GraphConstructor constr = new GraphConstructor();
